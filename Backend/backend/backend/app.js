13f49335302f07cfd4eca3bda7fc38a3480bea68
venkat009151-jpg <venkat009151@gmail.com>
2026-07-04 12:03:28 +0530

    Update app.js

--- Backend/backend/backend/app.js
+++ Backend/backend/backend/app.js
@@ -1,1 +1,28 @@
+const express = require("express");
+const cors = require("cors");
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Backend is running successfully!"
+    });
+});
+
+app.get("/api/info", (req, res) => {
+    res.json({
+        application: "EKS CI/CD Demo",
+        version: "1.0.0",
+        service: "Backend",
+        status: "Running"
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Backend running on port ${PORT}`);
+});
